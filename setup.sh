02fe1ab0552ce3f6ee844316f07a488cae992a66
Tang Qiao <tangqiao@rd.netease.com>
2011-12-14 11:31:41 +0800

    ADD: add setup.sh & adjust directory name

---
+++ setup.sh
@@ -0,0 +1,7 @@
+mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
+rm ~/Library/Developer/Xcode/UserData/CodeSnippets
+
+SRC_HOME=`pwd`
+ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+echo "done"
+
