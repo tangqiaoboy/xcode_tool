93aeea008f15c61f444e339e7471f4aa7af7198f
Tang Qiao <tangqiao@rd.netease.com>
2011-12-14 11:37:29 +0800

    MOD: remove useless shell

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+#! /bin/bash
 mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
-rm ~/Library/Developer/Xcode/UserData/CodeSnippets
+
+# rm ~/Library/Developer/Xcode/UserData/CodeSnippets
 
 SRC_HOME=`pwd`
 ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
