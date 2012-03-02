ebf1d9f601d15177199acc93f994a4fb9764b9a1
Tang Qiao <tangqiaoboy@gmail.com>
2012-03-02 18:14:21 +0800

    ADD: add system version compare macro.

---
+++ Macros/VersionCompare.h
@@ -0,0 +1,21 @@
+/*
+ *  System Versioning Preprocessor Macros
+ */ 
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
+/*
+ *  Usage sample:
+
+if (SYSTEM_VERSION_LESS_THAN(@"4.0")) {
+    ...
+}
+
+if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"3.1.1")) {
+    ...
+}
+
+*/ 
