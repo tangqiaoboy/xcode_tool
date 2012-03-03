95474a7686e4fcf6150663e9d638e8d89d2a891a
Tang Qiao <tangqiaoboy@gmail.com>
2012-03-03 12:25:18 +0800

    ADD: add UIConstant macro.

--- Macros/MacroUtils.h
+++ Macros/MacroUtils.h
@@ -3,6 +3,7 @@
 //
 // The macro is inspired from:
 //     http://www.yifeiyang.net/iphone-development-skills-of-the-debugging-chapter-2-save-the-log/
+
 #ifdef DEBUG
 #define debugLog(...) NSLog(__VA_ARGS__)
 #define debugMethod() NSLog(@"%s", __func__)
---
+++ Macros/UIConstants.h
@@ -0,0 +1,15 @@
+// Author: Tang Qiao
+// Date:   2012-3-3
+//
+
+// UIConstants provides contants variables for UI control.
+#define UI_NAVIGATION_BAR_HEIGHT    44
+#define UI_TAB_BAR_HEIGHT           49
+#define UI_STATUS_BAR_HEIGHT        20
+#define UI_SCREEN_WIDTH             320
+#define UI_SCREEN_HEIGHT            480
+
+#define UI_LABEL_LENGTH             200
+#define UI_LABEL_HEIGHT             15
+#define UI_LABEL_FONT_SIZE          12
+#define UI_LABEL_FONT               [UIFont systemFontOfSize:UI_LABEL_FONT_SIZE]
